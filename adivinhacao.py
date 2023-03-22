f650c28fc68fceb8c8e19121bc09aa6e28ffeb96
Douglas Leal <douglas.leal@live.com>
2023-03-21 22:55:57 -0300

    Limitando valores e validando chute

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,12 +9,17 @@ rodada_atual = 1
 while(rodada_atual <= total_tentativas):
     print(f"Tentativa {rodada_atual} de {total_tentativas}")
 
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número de 1 a 100: "))
 
     print(f"Você digitou: {chute}")
 
+    if(chute < 1 or chute > 100):
+        print("Valor inválido!")
+        continue
+
     if(numero_secreto == chute):
-        print("Você acertou")
+        print("Você acertou!!!")
+        break
     elif(numero_secreto < chute):
         print("Errado! Seu chute foi maior que o número secreto.")
     else:
@@ -22,4 +27,4 @@ while(rodada_atual <= total_tentativas):
 
     rodada_atual += 1
 
-print("Fim do Jogo!!!")
+print("Fim do Jogo.")
