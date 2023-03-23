7cd5fff1f312d2d52faac8d74a8a8be21866557f
Douglas Leal <douglas.leal@live.com>
2023-03-22 23:34:47 -0300

    Forca - limitando chutes e verificado vitória/derrota

--- forca.py
+++ forca.py
@@ -7,20 +7,30 @@ def jogar():
     letras_acertadas = ["_","_","_","_","_","_"]
     enforcado = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
     
     while(not enforcado and not acertou):
         chute = input("Digite uma letra: ").strip().lower()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra):
-                letras_acertadas[index] = letra
-            index += 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcado = erros == 6
+        acertou = "_" not in letras_acertadas     
         print(letras_acertadas)
-                
+
+    if(acertou):
+        print("Você ganhou!!!")
+    else:
+        print("Você perdeu!!!")           
 
 if(__name__ == "__main__"):
     jogar()
